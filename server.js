65bdf173fcf947065e31895230b07b3661e88f05
Michael Dai <sarahdj0917@gmail.com>
2016-09-21 13:35:35 +0800

    Process exit when mysql connect failed

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ var run = require('./lib/relay');
 
 function initApp() {
   var connection = mysql.createConnection(mysqlConfig);
-  connection.connect(function() {
+  connection.connect(function(err) {
+    if (err) {
+      console.error('error mysql connecting: ' + err.stack);
+      process.exit(1);
+    }
     run(connection);
   });
 }
